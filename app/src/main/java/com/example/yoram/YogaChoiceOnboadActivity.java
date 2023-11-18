c81f939a9139f246f51df617087f9c52415e94f0
braveJu <qkfmtpffhsk1109@gmail.com>
2023-11-18 13:32:08 +0900

    !making skeleton file!

---
+++ app/src/main/java/com/example/yoram/YogaChoiceOnboadActivity.java
@@ -0,0 +1,14 @@
+package com.example.yoram;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class YogaChoiceOnboadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_yoga_choice_onboad);
+    }
+}
