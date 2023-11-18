87f6fd64b54d63ea28e99c21a46411824af84240
braveJu <qkfmtpffhsk1109@gmail.com>
2023-11-18 16:48:59 +0900

    connection  code write

--- app/src/main/java/com/example/yoram/IntroduceOnboard.java
+++ app/src/main/java/com/example/yoram/IntroduceOnboard.java
@@ -2,13 +2,26 @@ package com.example.yoram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class IntroduceOnboard extends AppCompatActivity {
-
+    Button start_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_introduce_onboard);
+        start_button = (Button)findViewById(R.id.start_button);
+
+        start_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(IntroduceOnboard.this, YogaChoiceOnboadActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
