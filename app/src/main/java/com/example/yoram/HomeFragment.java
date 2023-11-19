88db94eacc8e7cb21816a2ed1cbdd95d0fa51a99
baravJu <qkfmtpffhsk119@gmail.com>
2023-11-19 14:12:30 +0900

    button onclicking~

--- app/src/main/java/com/example/yoram/HomeFragment.java
+++ app/src/main/java/com/example/yoram/HomeFragment.java
@@ -1,71 +1,97 @@
 package com.example.yoram;
 
-import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TimePicker;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HomeFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HomeFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    TimePicker timePicker;
+    Button buttonSetyoga;
+    Map<Button, Boolean> dayButtonStates = new HashMap<>();
+    Map<String, Calendar> dayTimeMap = new HashMap<>();
+    String offColor = "#FF4469";
+    String onColor = "#A2D5F2";
+    String[] days = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_yoram_home, container, false);
+
+        buttonSetyoga = view.findViewById(R.id.buttonSetyoga);
+        timePicker = view.findViewById(R.id.timePicker);
+
+        buttonSetyoga.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 버튼 클릭 로직
+            }
+        });
+
+        int[] buttonIds = {R.id.Mon, R.id.Tue, R.id.Wed, R.id.Thu, R.id.Fri, R.id.Sat, R.id.Sun};
+        for (int id : buttonIds) {
+            Button dayButton = view.findViewById(id);
+            dayButton.setOnClickListener(dayButtonClickListener);
+            dayButtonStates.put(dayButton, false);
         }
+
+        loadTimeData(); // 저장된 시간 데이터를 불러옵니다.
+
+        return view;
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_yoram_home, container, false);
-    }
+    private View.OnClickListener dayButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Button dayButton = (Button) v;
+            String day = dayButton.getText().toString();
+            boolean isOn = !dayButtonStates.get(dayButton);
+            dayButtonStates.put(dayButton, isOn);
+
+            dayButton.setBackground(new ColorDrawable(isOn ? Color.parseColor(onColor) : Color.parseColor(offColor)));
+            timePicker.setEnabled(isOn);
+
+            // 나머지 로직...
+        }
+    };
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
+    public void onPause() {
+        super.onPause();
+        saveTimeData(); // 시간 데이터를 저장합니다.
+    }
+
+    private void saveTimeData() {
+        SharedPreferences prefs = getActivity().getSharedPreferences("YoramHomePrefs", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        Gson gson = new Gson();
+        String dayTimeMapJson = gson.toJson(dayTimeMap);
+        editor.putString("dayTimeMap", dayTimeMapJson);
+        editor.apply();
+    }
+
+    private void loadTimeData() {
+        SharedPreferences prefs = getActivity().getSharedPreferences("YoramHomePrefs", Context.MODE_PRIVATE);
+        String dayTimeMapJson = prefs.getString("dayTimeMap", null);
+        if (dayTimeMapJson != null) {
+            Gson gson = new Gson();
+            Type type = new TypeToken<HashMap<String, Calendar>>(){}.getType();
+            dayTimeMap = gson.fromJson(dayTimeMapJson, type);
+        } else {
+            dayTimeMap = new HashMap<>();
+        }
     }
 }
--- app/src/main/java/com/example/yoram/YoramHome.java
+++ app/src/main/java/com/example/yoram/YoramHome.java
@@ -1,32 +1,87 @@
 package com.example.yoram;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-
-import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.FrameLayout;
-import android.widget.LinearLayout;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.widget.Button;
+import android.widget.TimePicker;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
 
 public class YoramHome extends AppCompatActivity {
-
-
-    FrameLayout home_ly;
-    BottomNavigationView bottomNavigationView;
-
+    TimePicker timePicker;
+    Button buttonSetyoga;
+    Map<Button, Boolean> dayButtonStates = new HashMap<>();
+    Map<String, Calendar> dayTimeMap = new HashMap<>();
+    String offColor = "#FF4469";
+    String onColor = "#A2D5F2";
+    String[] days = {"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_yoram_home);
-        //맨 처음 시작할 탭 설정
-    }
-}
+        buttonSetyoga = (Button)findViewById(R.id.buttonSetyoga);
+        timePicker = findViewById(R.id.timePicker);
 
+        buttonSetyoga.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setVisible(false);
+            }
+        });
+
+        // 모든 요일 버튼을 맵에 추가하고 초기 상태를 false로 설정
+        int[] buttonIds = {R.id.Mon, R.id.Tue, R.id.Wed, R.id.Thu, R.id.Fri, R.id.Sat, R.id.Sun};
+        for (int id : buttonIds) {
+            Button dayButton = findViewById(id);
+            dayButton.setOnClickListener(dayButtonClickListener);
+            dayButtonStates.put(dayButton, false);
+        }
+
+        // 초기에는 모든 요일에 대해 시간을 null로 설정
+        for (String day : days) {
+            dayTimeMap.put(day, null);
+        }
+    }
+
+
+
+    // 공통 클릭 리스너
+    private View.OnClickListener dayButtonClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Button dayButton = (Button) v;
+            String day = dayButton.getText().toString();
+            boolean isOn = !dayButtonStates.get(dayButton);
+            dayButtonStates.put(dayButton, isOn);
+
+            dayButton.setBackground(new ColorDrawable(isOn ? Color.parseColor(onColor) : Color.parseColor(offColor)));
+            timePicker.setEnabled(isOn);
+
+            if (isOn) {
+                Calendar calendar = dayTimeMap.get(day);
+                if (calendar != null) {
+                    // 이전에 설정된 시간이 있다면 TimePicker에 반영
+                    timePicker.setHour(calendar.get(Calendar.HOUR_OF_DAY));
+                    timePicker.setMinute(calendar.get(Calendar.MINUTE));
+                } else {
+                    // 시간이 설정되지 않았다면 현재 시간으로 설정
+                    Calendar now = Calendar.getInstance();
+                    timePicker.setHour(now.get(Calendar.HOUR_OF_DAY));
+                    timePicker.setMinute(now.get(Calendar.MINUTE));
+                }
+                timePicker.setOnTimeChangedListener((view, hourOfDay, minute) -> {
+                    // TimePicker에서 시간이 변경될 때마다 이를 저장
+                    Calendar selectedTime = Calendar.getInstance();
+                    selectedTime.set(Calendar.HOUR_OF_DAY, hourOfDay);
+                    selectedTime.set(Calendar.MINUTE, minute);
+                    dayTimeMap.put(day, selectedTime);
+                });
+            }
+        }
+    };
+}
