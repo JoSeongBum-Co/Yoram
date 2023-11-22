ae108f0d4e795d77c5baf6fa977df6e3b3b6b248
braveJu <qkfmtpffhsk1109@gmail.com>
2023-11-22 15:09:54 +0900

    camera good

--- app/src/main/java/com/example/yoram/YogaActivity.java
+++ app/src/main/java/com/example/yoram/YogaActivity.java
@@ -1,7 +1,14 @@
 package com.example.yoram;
 
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
 import android.os.Bundle;
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.Camera;
@@ -14,10 +21,13 @@ import androidx.camera.view.PreviewView;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
+
 import org.tensorflow.lite.Interpreter;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.util.concurrent.ExecutionException;
@@ -29,7 +39,9 @@ public class YogaActivity extends AppCompatActivity {
     private ProcessCameraProvider cameraProvider;
     private OverlayView overlayView;
     private Interpreter tflite;
-    private int count = 20;
+    private long lastAnalyzedTimestamp = 0;
+    private int count = 20; // 초기 카운트 값
+    private String targetPoseName = "left"; // 목표 요가 자세 이름
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,16 +51,13 @@ public class YogaActivity extends AppCompatActivity {
         overlayView = findViewById(R.id.overlayView);
         overlayView.setOverlayText("Count : " + count);
 
-        // TensorFlow Lite 모델 초기화
         try {
-            tflite = new Interpreter(loadModelFile("your_model.tflite"));
+            tflite = new Interpreter(loadModelFile("dongjumodel.tflite"));
         } catch (IOException e) {
             e.printStackTrace();
-            // 모델 파일 로딩 실패 처리
         }
 
-        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA)
-                == PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
             startCamera();
         } else {
             ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
@@ -56,11 +65,11 @@ public class YogaActivity extends AppCompatActivity {
     }
 
     private MappedByteBuffer loadModelFile(String modelFileName) throws IOException {
-        try (FileInputStream is = new FileInputStream(getAssets().openFd(modelFileName).getFileDescriptor())) {
-            FileChannel channel = is.getChannel();
+        try (FileInputStream fis = new FileInputStream(getAssets().openFd(modelFileName).getFileDescriptor())) {
+            FileChannel fileChannel = fis.getChannel();
             long startOffset = getAssets().openFd(modelFileName).getStartOffset();
             long declaredLength = getAssets().openFd(modelFileName).getDeclaredLength();
-            return channel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+            return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
         }
     }
 
@@ -77,43 +86,91 @@ public class YogaActivity extends AppCompatActivity {
 
     private void bindPreview() {
         Preview preview = new Preview.Builder().build();
+        CameraSelector cameraSelector = new CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_FRONT).build();
 
-        CameraSelector cameraSelector = new CameraSelector.Builder()
-                .requireLensFacing(CameraSelector.LENS_FACING_FRONT)
-                .build();
-
-        preview.setSurfaceProvider(viewFinder.getSurfaceProvider());
-
-        ImageAnalysis imageAnalysis =
-                new ImageAnalysis.Builder()
-                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                        .build();
+        ImageAnalysis imageAnalysis = new ImageAnalysis.Builder().setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST).build();
 
         imageAnalysis.setAnalyzer(ContextCompat.getMainExecutor(this), image -> {
-            // TODO: 이미지 프로세싱 및 모델 추론 코드 작성
-            // byte[] imageData = convertImageToByteArray(image); // 예시 함수, 이미지를 바이트 배열로 변환해야 함
+            long currentTime = System.currentTimeMillis();
+            // 마지막 분석 시간으로부터 1초가 지났는지 확인
+            if (currentTime - lastAnalyzedTimestamp >= 1000) { // 1000ms = 1초
+                byte[] imageData = convertImageToByteArray(image);
+                String poseName = runInference(imageData);
 
-            // TensorFlow Lite 모델 추론
-            // float[][] outputData = runInference(imageData);
+                if (poseName.equals(targetPoseName)) {
+                    runOnUiThread(() -> {
+                        count--;
+                    });
+                }
 
-            // UI 업데이트
-            // runOnUiThread(() -> {
-            // if (outputData[0][0] > SOME_CONFIDENCE_THRESHOLD) {
-            //     count--;
-            //     overlayView.setOverlayText("Count : " + count);
-            // }
-            // });
+                lastAnalyzedTimestamp = currentTime; // 현재 시간을 마지막 분석 시간으로 업데이트
+            }
 
-            image.close(); // 이미지 프로세싱이 끝나면 닫아주어야 함
+            image.close();
         });
 
-        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview, imageAnalysis);
+        cameraProvider.bindToLifecycle((LifecycleOwner) this, cameraSelector, preview, imageAnalysis);
+        preview.setSurfaceProvider(viewFinder.getSurfaceProvider());
     }
 
-    private float[][] runInference(byte[] imageData) {
-        float[][] outputData = new float[1][1]; // 모델의 출력 크기에 따라 변경될 수 있음
-        tflite.run(imageData, outputData);
-        return outputData;
+    private float[][] convertByteArrayToFloatArray(byte[] imageData) {
+        // 이미지 크기 및 채널 수에 따라 배열 크기를 조정합니다.
+        // 예: 224x224 크기의 컬러 이미지를 입력으로 사용하는 경우
+        int IMAGE_SIZE = 224;
+        int NUM_CHANNELS = 3;
+        float[][] floatArray = new float[1][IMAGE_SIZE * IMAGE_SIZE * NUM_CHANNELS];
+
+        for (int i = 0; i < imageData.length; i++) {
+            // 바이트 값을 0에서 255 범위에서 0.0에서 1.0 범위로 변환합니다.
+            int pixel = imageData[i] & 0xff;
+            floatArray[0][i * NUM_CHANNELS + 0] = pixel / 255.0f; // R 채널
+            floatArray[0][i * NUM_CHANNELS + 1] = pixel / 255.0f; // G 채널
+            floatArray[0][i * NUM_CHANNELS + 2] = pixel / 255.0f; // B 채널
+        }
+
+        return floatArray;
+    }
+
+    private String runInference(byte[] imageData) {
+        float[][] inputData = convertByteArrayToFloatArray(imageData);
+        float[][] outputData = new float[10][2]; // 모델의 출력 형식에 맞게 수정 필요
+        tflite.run(inputData, outputData);
+        return interpretOutput(outputData); // 모델의 출력을 해석하는 함수 필요
+    }
+
+    private byte[] convertImageToByteArray(ImageProxy image) {
+        ImageProxy.PlaneProxy[] planes = image.getPlanes();
+        ByteBuffer yBuffer = planes[0].getBuffer();
+        ByteBuffer uBuffer = planes[1].getBuffer();
+        ByteBuffer vBuffer = planes[2].getBuffer();
+
+        int ySize = yBuffer.remaining();
+        int uSize = uBuffer.remaining();
+        int vSize = vBuffer.remaining();
+
+        byte[] nv21 = new byte[ySize + uSize + vSize];
+
+        // Y, U, and V 버퍼를 하나의 배열(nv21)로 병합
+        yBuffer.get(nv21, 0, ySize);
+        vBuffer.get(nv21, ySize, vSize);
+        uBuffer.get(nv21, ySize + vSize, uSize);
+
+        // YUV -> Bitmap 변환
+        YuvImage yuvImage = new YuvImage(nv21, ImageFormat.NV21, image.getWidth(), image.getHeight(), null);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        yuvImage.compressToJpeg(new Rect(0, 0, image.getWidth(), image.getHeight()), 100, out);
+        byte[] imageBytes = out.toByteArray();
+
+        // Bitmap -> RGB 바이트 배열
+        Bitmap bitmap = BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        return stream.toByteArray();
+    }
+
+    private String interpretOutput(float[][] outputData) {
+        // TODO: 모델의 출력 데이터를 해석하여 자세 이름을 반환하는 로직 구현
+        return outputData[0][0] > outputData[0][1] ? "left" : "right";
     }
 
     @Override
@@ -135,7 +192,7 @@ public class YogaActivity extends AppCompatActivity {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 startCamera();
             } else {
-                // 권한이 거부됨
+                // 권한 거부 처리
             }
         }
     }
